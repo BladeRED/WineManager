49e8fcf69828f59284809414f0d5bc73d7a9ff1c
Julien Windows <juleray@msn.com>
2022-12-05 16:46:38 +0100

    Add of the controllers for Drawer and Cave

---
+++ WineManager/Controllers/CaveController.cs
@@ -0,0 +1,119 @@
+﻿using WineManager.Entities;
+using WineManager.IRepositories;
+//using WineManager.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using WineManager.IRepositories;
+
+namespace WineManager.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class CaveController : ControllerBase
+    {
+        ICaveRepository caveRepository;
+        readonly IWebHostEnvironment environment;
+        public CaveController(ICaveRepository caveRepository, IWebHostEnvironment environment)
+        {
+            this.caveRepository = caveRepository;
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cave>>> GetAllCaves()
+        {
+            var caves = await caveRepository.GetCavesAsync();
+
+            return Ok(caves);
+        }
+
+        /// <summary>
+        /// Get cave from Id
+        /// </summary>
+        /// <param name="id">Id cave</param>
+        /// <returns></returns>
+        /// 
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cave>> GetCave(int id)
+        {
+            return Ok(await caveRepository.GetByIdAsync(id));
+        }
+
+        [HttpGet("{id}")]
+        //[HttpGet("GetCaveWithDrawer/{id}")]
+        public async Task<ActionResult<Cave>> GetCaveWithDrawer(int id)
+        {
+            return Ok(await caveRepository.GetWithDrawerAsync(id));
+        }
+
+        [HttpGet("{id}")]
+        //[HttpGet("GetCaveWithDrawer/{id}")]
+        public async Task<ActionResult<Cave>> GetCaveWithUser(int id)
+        {
+            return Ok(await caveRepository.GetWithUserAsync(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(666)]
+
+
+        public async Task<ActionResult<Cave>> AddCave(Cave cave)
+        {
+
+          
+            var MyCave = new Cave()
+            {
+                CaveType = cave.CaveType,
+                Family = cave.Family,
+                Brand = cave.Brand,
+                Temperature = cave.Temperature,
+            };
+            var caveAddd = await caveRepository.AddCaveAsync(cave);
+
+            if (caveAddd == null)
+                return Problem("Error when creating cave, see log.");
+
+            //if (!string.IsNullOrEmpty(cave.Picture?.FileType) && cave.Picture.FileType.Length > 0)
+
+            //{// service IWebHostEnvironment
+            //    var path = Path.Combine(environment.WebRootPath, "Pictures/", cave.Picture.FileType);
+            //    using (FileStream stream = new FileStream(path, FileMode.Add)) { await cave.Picture.CopyToAsync(stream); stream.Close(); }
+            //}
+
+            return Ok(caveAddd);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(231)]
+
+        public async Task<ActionResult<Cave>> UpdateCave(Cave cave)
+        {
+            var caveUpdated = await caveRepository.UpdateCaveAsync(cave);
+
+            if (caveUpdated != null)
+                return Ok(caveUpdated);
+            else
+                return Problem("Cave was not updated, see log for details");
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(244)]
+
+        public async Task<ActionResult<Cave>> DeleteCave(int id)
+        {
+            var caveDeleted = await caveRepository.DeleteCaveAsync(id);
+
+            if (caveDeleted != null)
+                return Ok(caveDeleted);
+            else
+                return Problem("Cave was not deleted, see log for details");
+        }
+    }
+}
+
+
---
+++ WineManager/Controllers/DrawerController.cs
@@ -0,0 +1,104 @@
+﻿using WineManager.Entities;
+using WineManager.IRepositories;
+//using WineManager.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using WineManager.IRepositories;
+
+namespace WineManager.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class DrawerController : ControllerBase
+    {
+        IDrawerRepository drawerRepository;
+        readonly IWebHostEnvironment environment;
+        public DrawerController(IDrawerRepository drawerRepository, IWebHostEnvironment environment)
+        {
+            this.drawerRepository = drawerRepository;
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Drawer>>> GetAllDrawers()
+        {
+            var drawers = await drawerRepository.GetDrawersAsync();
+
+            return Ok(drawers);
+        }
+
+        /// <summary>
+        /// Get drawer from Id
+        /// </summary>
+        /// <param name="id">Id drawer</param>
+        /// <returns></returns>
+        /// 
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Drawer>> GetDrawer(int id)
+        {
+            return Ok(await drawerRepository.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(666)]
+
+
+        public async Task<ActionResult<Drawer>> AddDrawer(Drawer drawer)
+        {
+
+
+            var MyDrawer = new Drawer()
+            {
+                Level = drawer.Level,
+                MaxPosition = drawer.MaxPosition,
+              
+            };
+            var drawerAddd = await drawerRepository.AddDrawerAsync(drawer);
+
+            if (drawerAddd == null)
+                return Problem("Error when creating drawer, see log.");
+
+            //if (!string.IsNullOrEmpty(drawer.Picture?.FileType) && drawer.Picture.FileType.Length > 0)
+
+            //{// service IWebHostEnvironment
+            //    var path = Path.Combine(environment.WebRootPath, "Pictures/", drawer.Picture.FileType);
+            //    using (FileStream stream = new FileStream(path, FileMode.Add)) { await drawer.Picture.CopyToAsync(stream); stream.Close(); }
+            //}
+
+            return Ok(drawerAddd);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(231)]
+
+        public async Task<ActionResult<Drawer>> UpdateDrawer(Drawer drawer)
+        {
+            var drawerUpdated = await drawerRepository.UpdateDrawerAsync(drawer);
+
+            if (drawerUpdated != null)
+                return Ok(drawerUpdated);
+            else
+                return Problem("Drawer was not updated, see log for details");
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(244)]
+
+        public async Task<ActionResult<Drawer>> DeleteDrawer(int id)
+        {
+            var drawerDeleted = await drawerRepository.DeleteDrawerAsync(id);
+
+            if (drawerDeleted != null)
+                return Ok(drawerDeleted);
+            else
+                return Problem("Drawer was not deleted, see log for details");
+        }
+    }
+}
+
+
