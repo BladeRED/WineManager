06afb07b8937e27b3a04f56aac88dbcf01923729
vincent <baidem@gmail.com>
2022-12-05 10:29:14 +0100

    Drawer Navigation Keys Nulables

--- WineManager/Entities/Drawer.cs
+++ WineManager/Entities/Drawer.cs
@@ -5,8 +5,8 @@
         public int DrawerId { get; set; }
         public int Level { get; set; }
         public int MaxPosition { get; set; }
-        public List<Bottle> Bottles { get; set; }
-        public Cave Cave { get; set; }
-        public User User { get; set; }
+        public List<Bottle>? Bottles { get; set; }
+        public Cave? Cave { get; set; }
+        public User? User { get; set; }
     }
 }
