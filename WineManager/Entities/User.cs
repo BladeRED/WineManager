4816a51ff11d8b037fb5e95db75fa3d2d032bebe
Julien Windows <juleray@msn.com>
2022-12-05 09:59:00 +0100

    Creation of User Entity

---
+++ WineManager/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace BottleManager.Entities
+{
+    public class User
+    {
+
+        int UserId { get; set }
+
+        string Name { get; set }
+
+        string Email { get;set }
+
+
+
+    }
+}
