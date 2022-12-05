f73ea5b8c44d3c40274dc3f458cec5726b19bb5b
vincent <baidem@gmail.com>
2022-12-05 10:38:31 +0100

    BottleDto

---
+++ WineManager/DTO/BottleDto.cs
@@ -0,0 +1,15 @@
+﻿using BottleManager.Entities;
+
+namespace BottleManager.DTO
+{
+    public class BottleDto
+    {
+        public string Name { get; set; }
+        public int Vintage { get; set; }
+        public int StartKeepingYear { get; set; }
+        public int EndKeepingYear { get; set; }
+        public string Color { get; set; }
+        public string Designation { get; set; }
+        public string DrawerPosition { get; set; }
+    }
+}
