65b921b34d6db5d1b1dfddffaa7f17da1dbe95d8
vincent <baidem@gmail.com>
2022-12-05 10:18:31 +0100

    First Commit Drawer Entity

--- WineManager/Entities/Drawer.cs
+++ WineManager/Entities/Drawer.cs
@@ -2,5 +2,11 @@
 {
     public class Drawer
     {
+        public int DrawerId { get; set; }
+        public int Level { get; set; }
+        public int MaxPosition { get; set; }
+        public List<Bottle> Bottles { get; set; }
+        public Cave Cave { get; set; }
+        public User User { get; set; }
     }
 }
