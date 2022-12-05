095fd6bab4353354d7c32b7938df7885ad41e867
Andy-METHO <andy97412@live.fr>
2022-12-05 10:39:16 +0100

    Package

--- WineManager/Entities/Bottle.cs
+++ WineManager/Entities/Bottle.cs
@@ -2,6 +2,7 @@
 {
     public class Bottle
     {
+
         public int BottleId { get; set; }
         public string Name { get; set; }
         public int Vintage { get; set; }
@@ -10,6 +11,8 @@
         public string Color { get; set; }
         public string Designation { get; set; }
         public string DrawerPosition { get; set; }
+
+        //Navigation properties
         public Drawer? Drawer { get; set; }
         public User User { get; set; }
     }
