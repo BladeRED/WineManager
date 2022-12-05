04665cf2d3b8bfb6d9403a7bdeb9d06fdf7ea9f6
vincent <baidem@gmail.com>
2022-12-05 10:34:22 +0100

    DrawerDto

---
+++ WineManager/DTO/DrawerDto.cs
@@ -0,0 +1,10 @@
+﻿using BottleManager.Entities;
+
+namespace BottleManager.DTO
+{
+    public class DrawerDto
+    {
+        public int Level { get; set; }
+        public int MaxPosition { get; set; }
+    }
+}
