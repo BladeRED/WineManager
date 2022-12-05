c65f06af32dd20af93247e7ac43096c475695d55
Julien Windows <juleray@msn.com>
2022-12-05 10:12:02 +0100

    Initialization of User.DTO

--- WineManager/DTO/UserDTO.cs
+++ WineManager/DTO/UserDTO.cs
@@ -1,6 +1,9 @@
 ﻿namespace BottleManager.DTO
 {
-    public class UserDTO
+    public class UserDto
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
