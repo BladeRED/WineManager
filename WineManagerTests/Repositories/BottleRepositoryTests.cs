a9ebb40fbc6c77f9a6f64dd15bfa1de076ae555f
Julien Windows <juleray@msn.com>
2022-12-07 10:02:45 +0100

    Add Method test unit for Cave

---
+++ WineManagerTests/Repositories/BottleRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WineManager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineManager.Repositories.Tests
+{
+    [TestClass()]
+    public class BottleRepositoryTests
+    {
+        [TestMethod()]
+        public void AddBottleAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateBottleAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ WineManagerTests/Repositories/DrawerRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WineManager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineManager.Repositories.Tests
+{
+    [TestClass()]
+    public class DrawerRepositoryTests
+    {
+        [TestMethod()]
+        public void AddDrawerAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateDrawerAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
