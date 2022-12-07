55c9cebae9d146e18e3158b5ea8637ac226a7810
Julien Windows <juleray@msn.com>
2022-12-07 09:11:52 +0100

    Beginning of the unit test

---
+++ WineManagerTests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WineManager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WineManager.Contexts;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace WineManager.Repositories.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        [TestMethod()]
+        public async Task GetAllUsersAsyncTest()
+        {
+            var builder = new DbContextOptionsBuilder<WineManagerContext>().UseInMemoryDatabase("WineManagerTest");
+            var context = new WineManagerContext(builder.Options);
+            UserRepository MyGetAllTest = new UserRepository(context, null);
+
+            var MyList = await MyGetAllTest.GetAllUsersAsync();
+
+            context.Database.EnsureDeleted();
+            Assert.AreEqual(10, MyList.Count);
+        }
+
+        [TestMethod()]
+        public void AddUserAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
