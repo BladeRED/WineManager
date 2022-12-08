105a80e8fac31db76a40d2a70a643cdd84fd9df4
vincent <baidem@gmail.com>
2022-12-08 10:41:00 +0100

    Order Controllers

--- WineManager/Controllers/UserController.cs
+++ WineManager/Controllers/UserController.cs
@@ -55,116 +55,6 @@ namespace WineManager.Controllers
             return Ok(userDto);
         }
 
-        /// <summary>
-        /// Add a user
-        /// </summary>
-        /// <param name="Name"></param>
-        /// <param name="email"></param>
-        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(500)]
-        public async Task<ActionResult<UserDto>> AddUser([FromForm] UserPostDto userDto)
-        {
-            var userCreated = await userRepository.AddUserAsync(userDto);
-
-            if (userCreated != null)
-                return Ok(userCreated);
-            else
-                return Problem("User not created");
-        }
-
-        /// <summary>
-        /// Add a user
-        /// </summary>
-        /// <param name="Name"></param>
-        /// <param name="email"></param>
-        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
-        public async Task<IActionResult?> SignUp([FromForm] UserPostDto userDto, bool CGU)
-        {
-            if (CGU)
-            {
-                var user = await AddUser(userDto);
-                if (user != null)
-                {
-                    var userConnected = await userRepository.LoginUserAsync(userDto.Email, userDto.Password);
-                    return Ok($"{userConnected.Name} logged");
-                }
-                return BadRequest("Error in the request");
-            }
-            return Problem("CGU not accepted");
-        }
-
-        /// <summary>
-        /// Update a user from email
-        /// </summary>
-        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
-        /// <param name="userPutDto"></param>
-        /// <returns></returns>
-        [HttpPut]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(500)]
-        public async Task<ActionResult<UserDto>> UpdateUser([FromForm] UserPutDto userPutDto)
-        {
-            var userModified = await userRepository.UpdateUserAsync(userPutDto);
-
-            if (userModified != null)
-                return Ok(userModified);
-            else
-                return Problem("User not modified");
-        }
-
-        /// <summary>
-        /// Delete an User
-        /// </summary>
-        /// <param name="id">Find a user by its id and delete it</param>
-        /// <returns></returns>
-        [HttpDelete("{id}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<UserDto>> DeleteUser(int id)
-        {
-            if (id < 1)
-            {
-                return BadRequest("No valuable id found in the request");
-            }
-            var userRemoved = await userRepository.DeleteUserAsync(id);
-            if (userRemoved != null)
-                return Ok(userRemoved);
-            else
-                return NotFound("The User is not found.");
-        }
-
-        /// <summary>
-        /// Get user from id with his bottles
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<ActionResult<UserDtoGet?>> GetUserWithBottles(int id)
-        {
-            if (id < 1)
-            {
-                return BadRequest("No valuable id found in the request");
-            }
-            var userDto = await userRepository.GetUserWithBottlesAsync(id);
-            if (userDto == null)
-                return NotFound("The User is not found.");
-            else
-                return Ok(userDto);
-        }
 
         /// <summary>
         /// Get user from id with his drawers
@@ -286,6 +176,28 @@ namespace WineManager.Controllers
         }
 
         /// <summary>
+        /// Get user from id with his bottles
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<UserDtoGet?>> GetUserWithBottles(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("No valuable id found in the request");
+            }
+            var userDto = await userRepository.GetUserWithBottlesAsync(id);
+            if (userDto == null)
+                return NotFound("The User is not found.");
+            else
+                return Ok(userDto);
+        }
+
+        /// <summary>
         /// Login of a user from email and password
         /// </summary>
         /// <param name="login"></param>
@@ -312,6 +224,95 @@ namespace WineManager.Controllers
         {
             await HttpContext.SignOutAsync();
             return Ok("Logout");
+        }
+
+        /// <summary>
+        /// Add a user
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="email"></param>
+        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<UserDto>> AddUser([FromForm] UserPostDto userDto)
+        {
+            var userCreated = await userRepository.AddUserAsync(userDto);
+
+            if (userCreated != null)
+                return Ok(userCreated);
+            else
+                return Problem("User not created");
+        }
+
+        /// <summary>
+        /// Add a user
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="email"></param>
+        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult?> SignUp([FromForm] UserPostDto userDto, bool CGU)
+        {
+            if (CGU)
+            {
+                var user = await AddUser(userDto);
+                if (user != null)
+                {
+                    var userConnected = await userRepository.LoginUserAsync(userDto.Email, userDto.Password);
+                    return Ok($"{userConnected.Name} logged");
+                }
+                return BadRequest("Error in the request");
+            }
+            return Problem("CGU not accepted");
+        }
+
+        /// <summary>
+        /// Update a user from email
+        /// </summary>
+        /// <param name="birthDate"> format example: "2000-05-23" (without the string on SWAGGER) </param>
+        /// <param name="userPutDto"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<UserDto>> UpdateUser([FromForm] UserPutDto userPutDto)
+        {
+            var userModified = await userRepository.UpdateUserAsync(userPutDto);
+
+            if (userModified != null)
+                return Ok(userModified);
+            else
+                return Problem("User not modified");
+        }
+
+        /// <summary>
+        /// Delete an User
+        /// </summary>
+        /// <param name="id">Find a user by its id and delete it</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<UserDto>> DeleteUser(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("No valuable id found in the request");
+            }
+            var userRemoved = await userRepository.DeleteUserAsync(id);
+            if (userRemoved != null)
+                return Ok(userRemoved);
+            else
+                return NotFound("The User is not found.");
         }
     }
 }
